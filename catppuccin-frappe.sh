8bc6af30c2985ed377453bc718adb8fa5aadd111
mkutay <me@mkutay.dev>
2023-07-13 18:56:13 +0300

    new yabai theme

---
+++ catppuccin-frappe.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+source ~/.config/colors/catppuccin-frappe.sh
+
+export yabai_active_border_color="$catppuccin_frappe_mauve"
+export yabai_normal_border_color="$catppuccin_frappe_surface2"
---
+++ nord-dark.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+source ~/.config/colors/nord.sh
+
+export yabai_active_border_color="0xffb7c2d7" # a more subtle version of nord4
+export yabai_normal_border_color="$nord0"

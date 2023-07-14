21213b2d0a2208052e129c63f255ea74e083f181
mkutay <me@mkutay.dev>
2023-07-14 19:07:55 +0300

    new yabai theme and settings

---
+++ catppuccin-latte.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+source ~/.config/colors/catppuccin-latte.sh
+
+export yabai_active_border_color="$catppuccin_latte_mauve"
+export yabai_normal_border_color="$catppuccin_latte_base"
+
--- colors.sh
+++ colors.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-source ~/.config/yabai/catppuccin-frappe.sh
+source ~/.config/yabai/catppuccin-latte.sh
